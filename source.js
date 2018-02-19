194c40f320a65ab55bef667961dacec2577f2abd
Jared <jlgrimes@umich.edu>
2018-02-19 10:19:57 -0500

    I forgot what I added

--- source.js
+++ source.js
@@ -1,52 +1,70 @@
 var app = angular.module('app', []);
-app.controller('ctrl', function($scope) {
+app.controller('ctrl', function ($scope) {
+    function data(foo) {
+        return foo;
+    }
+
     // Hypergeometric JS from http://www.math.ucla.edu/~tom/distributions/Hypergeometric.html
     $scope.nn = 60; // 60 cards in deck
-    $scope.n = 7;   // 7 cards in starting hand
-    $scope.x = 0;
-    
+    $scope.n = 8; // 7 cards in starting hand + draw for turn
+    $scope.x = 0; // We want the odds of getting 
+
     $scope.brigette = 2;
     $scope.lele = 3;
     $scope.ball = 4;
-    
-    $scope.compute = function(x, n, nn) {
-        var odds = 1 - $scope.hyp(x, n, nn);
-        // initial odds are the odds of having Brigette, Ultra Ball, or Tapu Lele-GX in starting hand
-        
-        odds -= (1 - $scope.hyp($scope.brigette, 6, 60));  
-        // odds of prizing all Brigette
-        
-        if ($scope.lele > 1)
-            odds -= ((1 - $scope.hyp($scope.lele, 7, 60)) * $scope.hyp($scope.lele - 1, 6, 60))
-        // odds of starting Lele and prizing the other(s)
-        
-        return (100 * odds).toFixed(2);
-        // return the odds in a percentage with floating point 2
-    }
 
-    $scope.hyp = function(x, n, nn) {
-        var b = 0, l = 0, ba = 0;
-        if ($scope.brigette != "")
+    $scope.total = function (variance) {
+        var b = 0,
+            l = 0,
+            ba = 0;
+
+        var ifBrigette = !($scope.brigette == "" || $scope.brigette == 0),
+            ifLele = !($scope.lele == "" || $scope.lele == 0),
+            ifBall = !($scope.ball == "" || $scope.ball == 0);
+        // Accounts for if the field is left blank or inputted explicitly as 0
+        // We could set the value to zero, but the way angular deals with setting the output to zero when backspaced is rather annoying
+
+        if (ifBrigette)
             b = parseInt($scope.brigette);
-        // if Brigette count is valid, set b (Brigette count) to user input
-        // if it's not valid, b will stay equal to zero instead of trying to parseInt() ""
-        
-        if ($scope.lele != "" && $scope.brigette != "")
+        // If Brigette count is valid, set b (Brigette count) to user input
+        // If it's not valid, b will stay equal to zero instead of trying to parseInt() ""
+
+        if (ifBrigette && ifLele)
             l = parseInt($scope.lele);
-        // if Lele count is valid, set l (Lele count) to user input
-        // if it's not valid, l will stay equal to zero instead of trying to parseInt() ""
-        
-        if ($scope.ball != "" && $scope.lele != "" && $scope.brigette != "")
+        // If Lele count is valid, set l (Lele count) to user input
+        // If it's not valid, l will stay equal to zero instead of trying to parseInt() ""
+
+        if (ifBall && ifLele && ifBrigette)
             ba = parseInt($scope.ball);
-        // if Ultra Ball count is valid, set ba (Ultra Ball count) to user input
-        // if it's not valid, ba will stay equal to zero instead of trying to parseInt() ""
-        
+        // If Ultra Ball count is valid, set ba (Ultra Ball count) to user input
+        // If it's not valid, ba will stay equal to zero instead of trying to parseInt() ""
+
         var m = b + l + ba;
         // m, the total valid sample size, is equal to the sum of the Brigette count, Lele count, and Ultra Ball count
-        
-        // The rest of this algorithm is from https://gist.github.com/trevnorris/c39ac96740842e05303f
-        // implementation found at http://www.math.ucla.edu/~tom/distributions/Hypergeometric.html
-        
+
+        if (m)
+            m += variance;
+        // if m isn't zero, apply variance
+
+        var odds = 1 - $scope.hyp($scope.x, $scope.n, m, $scope.nn);
+        // Initial odds are the odds of having Brigette, Ultra Ball, or Tapu Lele-GX in starting hand
+
+        if ($scope.brigette <= 6)
+            odds -= (1 - $scope.hyp($scope.brigette, 6, $scope.brigette, 60));
+        // Odds of prizing all Brigette
+
+        if ($scope.lele > 1)
+            odds -= ((1 - $scope.hyp($scope.lele, 7, m, 60)) * $scope.hyp($scope.lele - 1, 6, m, 60));
+        // Odds of starting Lele and prizing the other(s)
+
+        return (100 * odds).toFixed(2);
+        // Return the odds in a percentage with floating point 2
+    }
+
+    $scope.hyp = function (x, n, m, nn) {
+        // This algorithm is from https://gist.github.com/trevnorris/c39ac96740842e05303f
+        // Implementation found at http://www.math.ucla.edu/~tom/distributions/Hypergeometric.html
+
         var nz, mz;
         // best to have n<m
         if (m < n) {
@@ -56,14 +74,14 @@ app.controller('ctrl', function($scope) {
             nz = n;
             mz = m
         }
-        var h=1;
-        var s=1;
-        var k=0;
-        var i=0;
+        var h = 1;
+        var s = 1;
+        var k = 0;
+        var i = 0;
         while (i < x) {
             while (s > 1 && k < nz) {
                 h = h * (1 - mz / (nn - k));
-                s = s * (1 -mz / (nn - k));
+                s = s * (1 - mz / (nn - k));
                 k = k + 1;
             }
             h = h * (nz - i) * (mz - i) / (i + 1) / (nn - nz - mz + i + 1);
